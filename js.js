60bf40908be578a614f90bdd4535a98c692c8540
domi15 <81708998+domi15@users.noreply.github.com>
2023-01-02 15:45:37 +0100

    Update js.js
    Pretvorba u fiksnu ratu

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 function eur(){
     var number = prompt("Unesite iznos u Eurima")
-    document.getElementById("HRK").innerHTML = 7.511564*number
+    document.getElementById("HRK").innerHTML = 7.53450*number
     document.getElementById('EUR').innerHTML = number
 }
 function hrk(){
     var number = prompt("Unesite iznos u Kunama")
-    document.getElementById("EUR").innerHTML = number/7.511564
+    document.getElementById("EUR").innerHTML = number/7.53450
     document.getElementById('HRK').innerHTML = number
 }
